08ba9143c2f01fcc203511ec65e55eba48d205d9
Matt Wilkin <mwilkin22@gmail.com>
2016-08-11 22:19:45 -0700

    create app component view; name, description, calorie count.

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,9 +5,20 @@ import { Component } from 'angular2/core';
   template: `
     <div class="container">
       <h1>Meal Tracker</h1>
-    </div>  
+      <h3>{{  meal.name }}</h3>
+      <h3>{{  meal.description }}</h3>
+      <h3>{{  meal.calories }}</h3>
+    </div>
   `
 })
 export class AppComponent {
+  public meal: Meal;
+  constructor(){
+    this.meal = new Meal("Hamburgers!", "No bun!", 1500, 0);
+  }
+}
+export class Meal {
+  constructor(public name: string, public description: string, public calories: number, public id: number){
 
+  }
 }
