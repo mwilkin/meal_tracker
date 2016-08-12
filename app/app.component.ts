c4acbcc4684053963d1e2551321e7a4c90e57175
Matt Wilkin <mwilkin22@gmail.com>
2016-08-11 22:32:19 -0700

    click event emitter established.

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,16 +5,23 @@ import { Component } from 'angular2/core';
   template: `
     <div class="container">
       <h1>Meal Tracker</h1>
-      <h3>{{  meal.name }}</h3>
-      <h3>{{  meal.description }}</h3>
-      <h3>{{  meal.calories }}</h3>
+      <h3 *ngFor="#meal of meals" (click)="mealWasSelected(meal)">Meal name: {{  meal.name }}. Description: {{  meal.description }}. Calorie count: {{  meal.calories }}</h3>
     </div>
   `
 })
 export class AppComponent {
-  public meal: Meal;
+  public meals: Meal[];
   constructor(){
-    this.meal = new Meal("Hamburgers!", "No bun!", 1500, 0);
+    this.meals = [
+      new Meal("Hamburgers!", "No bun!", 1500, 0),
+      new Meal("Chocolate Shake", "No whipcream!", 600, 1),
+      new Meal("Apple", "I'm still hungry", 150, 2),
+      new Meal("Kind Bar", "Dark Chocolate, nuts & sea salt. Yum!", 200, 3),
+      new Meal("More Hamburgers!", "No bun!", 1500, 0)
+    ];
+  }
+  mealWasSelected(clickedMeal: Meal): void {
+    console.log(clickedMeal);
   }
 }
 export class Meal {
